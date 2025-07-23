9b46bf50e88261a29f9cd5733e290aef0c2de6f5
samarshi baidya <samarshi19@gmail.com>
2025-07-23 18:29:54 +0530

    created a basic calculator flutter app using the dart tested code

---
+++ test_cases/unit_test_case_4.dart
@@ -0,0 +1,76 @@
+import '../src/kata_calculator_step_4.dart';
+
+// to test failed scenario for line 41, and 48 uncomment the line below and comment the line above
+// import '../src/kata_calculator_step_2.dart';
+
+void main() {
+  testAdditionOfNumbers();
+}
+
+void testAdditionOfNumbers() {
+  print('Running tests for addition of multiple numbers');
+
+  try {
+    assert(add("1,2") == 3);
+    print('Test passed: 1 + 2 = 3');
+  } catch (e) {
+    print('Test failed: Expected 3 but got ${add("1,2")} - $e');
+  }
+
+  try {
+    assert(add("") == 0);
+    print('Test passed: "" = 0');
+  } catch (e) {
+    print('Test failed: Expected 0 but got - $e');
+  }
+
+  try {
+    assert(add("1") == 1);
+    print('Test passed: 1 = 1');
+  } catch (e) {
+    print('Test failed: Expected 1 but got - $e');
+  }
+
+  try {
+    assert(add("1,2,3") == 6);
+    print('Test passed: 1 + 2 + 3 = 6');
+  } catch (e) {
+    print('Test failed: Expected 6 but got - $e');
+  }
+
+  try {
+    assert(add("10,20,30,40") == 100);
+    print('Test passed: 10 + 20 + 30 + 40 = 100');
+  } catch (e) {
+    print('Test failed: Expected 100 but got  - $e');
+  }
+
+  try {
+    assert(add("100\n200\n300,10") == 610);
+    print('Test passed: 100 + 200 + 300 + 10 = 610');
+  } catch (e) {
+    print('Test failed: Expected 610 but got - $e');
+  }
+
+   try {
+    assert(add("10\n20\n30\n40") == 100);
+    print('Test passed: 10 + 20 + 30 + 40 = 100');
+  } catch (e) {
+    print('Test failed: Expected 100 but got  - $e');
+  }
+
+   try {
+    assert(add("//*\n10*20*30,40") == 100);
+    print('Test passed: 10 + 20 + 30 + 40 = 100');
+  } catch (e) {
+    print('Test failed: Expected 100 but got  - $e');
+  }
+
+  try {
+      assert(add("//;\n10;20;30,40") == 100);
+      print('Test passed: 10 + 20 + 30 + 40 = 100');
+    } catch (e) {
+      print('Test failed: Expected 100 but got  - $e');
+  }
+
+}
